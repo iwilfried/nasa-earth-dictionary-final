fa7c551ab6b601acb5f2c3456def9b889d7d4440
Wilfried Ifland <wilfried.ifland@gmail.com>
2022-11-30 16:46:13 +0100

    delete words

--- lib/screens/welcome_page/main_screen.dart
+++ lib/screens/welcome_page/main_screen.dart
@@ -15,105 +15,104 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body:
-         Container(
-          width: width,
-          height: height,
-          color: Theme.of(context).primaryColor,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                //crossAxisAlignment: CrossAxisAlignment.stretch, adjustment
-                children: [
-                  const SizedBox(height: 50),
-                  Image.asset('assets/images/smallview.png'),
-                  const Text("Welcome",
-                      style: TextStyle(
-                        fontSize: 24,
+      body: Container(
+        width: width,
+        height: height,
+        color: Theme.of(context).primaryColor,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.stretch, adjustment
+              children: [
+                const SizedBox(height: 80),
+                Image.asset('assets/images/smallview.png'),
+                const Text("Welcome",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    )),
+                const SizedBox(height: 50),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                  child: Text(
+                    " Kaufleute im \n E-COMMERCE\n\nGLOSSARY",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.oswald(
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.normal,
                         color: Colors.white,
-                      )),
-                  const SizedBox(height: 50),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-                    child: Text(
-                      " Kaufleute im \n E-COMMERCE\n\nGLOSSARY",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.oswald(
-                        textStyle: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: Colors.white,
-                          fontSize: 30,
-                        ),
+                        fontSize: 36,
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    height: 35,
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Text(
+                    'Use this glossary to learn and understand vocabulary, terms and\n  definitions.',
+                    maxLines: 5,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(30.0),
-                    child: Text(
-                      'Use this glossary to learn and understand vocabulary, terms and\n  definitions.',
-                      maxLines: 5,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.white),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.deepOrange),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const HomePage(),
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.deepOrange),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomePage(),
+                            ),
+                          );
+                        },
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                "WEITER",
+                                style: TextStyle(color: Colors.white),
                               ),
-                            );
-                          },
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: const [
-                              Padding(
-                                padding: EdgeInsets.only(left: 8.0),
-                                child: Text(
-                                  "WEITER",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.white,
-                                size: 20,
-                              )
-                            ],
-                          ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.white,
+                              size: 20,
+                            )
+                          ],
                         ),
                       ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
-
+      ),
     );
   }
 }
